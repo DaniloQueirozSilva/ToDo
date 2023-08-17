be6c5695bc038ccf83fb9ba797fc9690dd8f47a0
Danilo Queiroz <daniloqueirozsilva05@gmail.com>
2023-08-16 22:52:11 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("TrustServerCertificate=True; Persist Security Info=False;Integrated Security=true;Initial Catalog=ToDo;server=DESKTOP-3PVVSJJ\\SERVERSQL");
+
+
+        }
+
+    }
+}
